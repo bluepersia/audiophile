6ab19a85f916d470d89c25c2b849370485e0015e
Marco Enrique <fuzzyslash@hotmail.com>
2026-06-24 15:17:00 -0600

    Setup Tanstack React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import type { JSX } from "react/jsx-runtime";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient: QueryClient = new QueryClient();
 
 function App(): JSX.Element {
   const message = "Hello World";
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="container">
-              <h1>{message}</h1>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="container">
+                <h1>{message}</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
