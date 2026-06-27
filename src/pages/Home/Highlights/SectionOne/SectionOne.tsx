9abdfb6fefa7382033f755d4d4b678eea159b591
Marco Enrique <fuzzyslash@hotmail.com>
2026-06-27 14:46:35 -0600

    Extract useProductQuery

--- src/pages/Home/Highlights/SectionOne/SectionOne.tsx
+++ src/pages/Home/Highlights/SectionOne/SectionOne.tsx
@@ -1,58 +1,36 @@
 import type { JSX } from "react/jsx-runtime";
 import type { SectionRendererProps } from "../SectionRenderer.types";
 import styles from "./SectionOne.module.scss";
-import { useQuery } from "@tanstack/react-query";
-import { getProduct } from "../../../../api/products.api";
 import Btn from "../../../../components/Btn/Btn";
-import Spinner from "../../../../components/Spinner/Spinner";
-import Error from "../../../../components/Error/Error";
+import useProductQuery from "../../../../hooks/useProductQuery";
 
 export default function SectionOne({
   section,
 }: SectionRendererProps): JSX.Element {
-  const {
-    data: product,
-    isFetching,
-    error,
-  } = useQuery({
-    queryKey: ["products", section.productSlug],
-    queryFn: () => getProduct(section.productSlug),
-  });
+  const { sectionJSX } = useProductQuery(section.productSlug, (product) => (
+    <>
+      <picture className={styles.picture}>
+        <source srcSet={section.image.desktop} media="(min-width: 1200px)" />
+        <source srcSet={section.image.tablet} media="(min-width: 768px)" />
+        <img
+          src={section.image.mobile}
+          alt={section.alt}
+          className={styles.img}
+        />
+      </picture>
+      <div className={styles.content}>
+        <h2 className={styles.name}>{section.alias || product.name}</h2>
+        <p className={styles.desc}>{section.description}</p>
+        <Btn
+          color="dark"
+          className={styles.btn}
+          to={`products/${product.slug}`}
+        >
+          See Product
+        </Btn>
+      </div>
+    </>
+  ));
 
-  function render(): JSX.Element {
-    if (isFetching) {
-      return <Spinner />;
-    }
-
-    if (error) {
-      return <Error error={error} />;
-    }
-
-    return (
-      <>
-        <picture className={styles.picture}>
-          <source srcSet={section.image.desktop} media="(min-width: 1200px)" />
-          <source srcSet={section.image.tablet} media="(min-width: 768px)" />
-          <img
-            src={section.image.mobile}
-            alt={section.alt}
-            className={styles.img}
-          />
-        </picture>
-        <div className={styles.content}>
-          <h2 className={styles.name}>{product.name}</h2>
-          <p className={styles.desc}>{section.description}</p>
-          <Btn
-            color="dark"
-            className={styles.btn}
-            to={`products/${product.slug}`}
-          >
-            See Product
-          </Btn>
-        </div>
-      </>
-    );
-  }
-
-  return <article className={styles["section-one"]}>{render()}</article>;
+  return <article className={styles["section-one"]}>{sectionJSX}</article>;
 }
--- src/pages/Home/Highlights/SectionTwo/SectionTwo.tsx
+++ src/pages/Home/Highlights/SectionTwo/SectionTwo.tsx
@@ -1,53 +1,31 @@
 import type { JSX } from "react/jsx-runtime";
 import type { SectionRendererProps } from "../SectionRenderer.types";
 import styles from "./SectionTwo.module.scss";
-import { useQuery } from "@tanstack/react-query";
-import { getProduct } from "../../../../api/products.api";
 import Btn from "../../../../components/Btn/Btn";
-import Spinner from "../../../../components/Spinner/Spinner";
-import Error from "../../../../components/Error/Error";
+import useProductQuery from "../../../../hooks/useProductQuery";
 
 export default function SectionTwo({
   section,
 }: SectionRendererProps): JSX.Element {
-  const {
-    data: product,
-    isFetching,
-    error,
-  } = useQuery({
-    queryKey: ["products", section.productSlug],
-    queryFn: () => getProduct(section.productSlug),
-  });
+  const { sectionJSX } = useProductQuery(section.productSlug, (product) => (
+    <>
+      <div className={styles.content}>
+        <h2 className={styles.name}>{section.alias || product.name}</h2>
+        <Btn color="transparent" to={`products/${product.slug}`}>
+          See Product
+        </Btn>
+      </div>
+      <picture className={styles.picture}>
+        <source srcSet={section.image.desktop} media="(min-width:1200px)" />
+        <source srcSet={section.image.tablet} media="(min-width:768px)" />
+        <img
+          src={section.image.mobile}
+          alt={section.alt}
+          className={styles.img}
+        />
+      </picture>
+    </>
+  ));
 
-  function render(): JSX.Element {
-    if (isFetching) {
-      return <Spinner />;
-    }
-
-    if (error) {
-      return <Error error={error} />;
-    }
-
-    return (
-      <>
-        <div className={styles.content}>
-          <h2 className={styles.name}>{section.alias || product.name}</h2>
-          <Btn color="transparent" to={`products/${product.slug}`}>
-            See Product
-          </Btn>
-        </div>
-        <picture className={styles.picture}>
-          <source srcSet={section.image.desktop} media="(min-width:1200px)" />
-          <source srcSet={section.image.tablet} media="(min-width:768px)" />
-          <img
-            src={section.image.mobile}
-            alt={section.alt}
-            className={styles.img}
-          />
-        </picture>
-      </>
-    );
-  }
-
-  return <article className={styles["section-two"]}>{render()}</article>;
+  return <article className={styles["section-two"]}>{sectionJSX}</article>;
 }
